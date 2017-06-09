1861cad9df5c26eb59d487e4e728a8c17d40bbcf
Mark Riedesel <mark@klowner.com>
2017-06-08 23:19:24 -0500

    tweak

--- archlinux-gcc-x64/assets/distcc-enable.sh
+++ archlinux-gcc-x64/assets/distcc-enable.sh
@@ -2,5 +2,6 @@
 export PATH="/usr/lib/colorgcc/bin:$PATH"
 export CCACHE_PATH="/usr/bin"
 export CCACHE_DIR="/mnt/cache"
+export CGCC_FORCE_COLOR=true
 sed -e 's/\/usr\//\/usr\/lib\/ccache\//' -i /etc/colorgcc/colorgccrc
 sed -e 's/^#\([^:]*\)\: /\1: /' -i /etc/colorgcc/colorgccrc
